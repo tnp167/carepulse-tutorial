a997bb172bcd1aafe71f427912179e5b4cb33742
tnp_167 <tnp_167@outlook.com>
2024-08-12 10:36:38 +0100

    Add appointment table

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,4 +1,6 @@
 import StatCard from "@/components/StatCard";
+import { columns, Payment } from "@/components/table/columns";
+import { DataTable } from "@/components/table/DataTable";
 import { getRecentAppointmentList } from "@/lib/actions/appointment.actions";
 import Image from "next/image";
 import Link from "next/link";
@@ -48,6 +50,7 @@ const Admin = async () => {
             icon="/assets/icons/cancelled.svg"
           />
         </section>
+        <DataTable columns={columns} data={appointments.documents} />
       </main>
     </div>
   );
